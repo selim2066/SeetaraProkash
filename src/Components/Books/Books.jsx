9828cdada2c9b9e8117a05f17feb60f1dbb6274c
selim2066 <mdselimreza2066@gmail.com>
2025-08-01 12:39:26 +0600

    books grid fetch done

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -9,8 +9,8 @@ const Books = () => {
       .then((data) => setBooks(data));
   }, []);
   return (
-    <div>
-      <h1 className="text-5xl text-center">Books</h1>
+    <div className="my-20">
+      <h1 className="text-5xl text-center py-10">Books</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
         {books.map((book) => (
